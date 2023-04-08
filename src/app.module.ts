0e34199d709ce0e51626b26d7a2d5f673dd399c2
Georgiy <georgiy.pyantkovsky.ukr@gmail.com>
2023-04-08 14:56:44 -0400

    feat: added connection to Mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
-import { Module } from '@nestjs/common';
-import { ApiModule } from './api/api.module';
-import { DomainModule } from './domain/domain.module';
+import { Module } from "@nestjs/common";
+import { ApiModule } from "./api/api.module";
+import { DomainModule } from "./domain/domain.module";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [ApiModule, DomainModule],
+    imports: [
+        MongooseModule.forRoot(
+            `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}.p2scv.mongodb.net/${process.env.MONGO_COLLECTION_NAME}?retryWrites=true&w=majority`
+        ),
+        ApiModule,
+        DomainModule,
+    ],
 })
 export class AppModule {}
