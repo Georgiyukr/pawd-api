cef3574106cbee4365596cd2a71687d9bbb14661
Georgiy <georgiy.pyantkovsky.ukr@gmail.com>
2023-04-08 15:32:03 -0400

    feat: registered config module. Able to read env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from "@nestjs/common";
 import { ApiModule } from "./api/api.module";
 import { DomainModule } from "./domain/domain.module";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
     imports: [
+        ConfigModule.forRoot({ envFilePath: `${process.env.NODE_ENV}.env` }),
         MongooseModule.forRoot(
-            `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}.p2scv.mongodb.net/${process.env.MONGO_COLLECTION_NAME}?retryWrites=true&w=majority`
+            `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.8zakhvw.mongodb.net/?retryWrites=true&w=majority`,
+            { dbName: process.env.MONGO_DATABASE_NAME }
         ),
         ApiModule,
         DomainModule,
