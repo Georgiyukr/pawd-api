a38a9683d280954b68f031de7a65a7498f722240
Georgiy Pyantkovsky <georgiy@Georgiys-MacBook-Pro.local>
2025-06-30 22:54:39 -0400

    feat(ci): created git actions workflow to deploy to dev

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    if (process.env.MONGO_USERNAME) {
+        console.log('✅ MONGO_USERNAME is available')
+    } else {
+        console.warn('❌ MONGO_USERNAME is missing')
+    }
     const port = process.env.PORT || 8080
     await app.listen(port)
 }
